b4d5079a6b2b1a613af08d439b2e08eb73624c06
seoil <seoil@HH311-14>
2017-11-23 11:27:45 +0900

    create

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -3,10 +3,8 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-<<<<<<< HEAD
-=======
 		System.out.println("Hello Everyone");
->>>>>>> refs/heads/hotfix
+		
 
 	}
 
